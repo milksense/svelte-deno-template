1307a1cffa7c60a9757a25c5f50f3ed55336c600
milksense <29128703+milksense@users.noreply.github.com>
2024-12-12 03:28:16 +0300

    Initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import "../app.css";
+
+	let { children } = $props();
+</script>
+
+<div class="core">
+	<main class="core__content">
+		{@render children()}
+	</main>
+</div>
+
+<style>
+	.core {
+		display: grid;
+		width: 100%;
+		height: 100vh;
+		grid-template-areas: ". content .";
+		grid-template-columns: 1fr minmax(auto, 450px) 1fr;
+	}
+
+	.core__content {
+		grid-area: content;
+		align-content: center;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="border-2">
+	<h1 class="m-2 font-mono text-2xl">
+		SvelteKit + Tailwind using
+		<span class="text-neon-red">Deno!</span>
+	</h1>
+</div>
